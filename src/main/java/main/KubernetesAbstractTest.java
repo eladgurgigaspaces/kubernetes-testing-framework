232eef89f750127987572e5fa1a4140f4cd2d391
Elad Gur <elad.gur@gigaspaces.com>
2019-02-04 17:58:58 +0200

    Improve KubernetesAbstractTest

--- src/main/java/main/KubernetesAbstractTest.java
+++ src/main/java/main/KubernetesAbstractTest.java
@@ -4,14 +4,18 @@ import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
 
+import static main.HelmUtils.installChartWithArgs;
 import static main.HelmUtils.removeAllChartsInstances;
+import static main.KubernetesUtils.assertNoRunningPods;
+import static main.KubernetesUtils.assertPodsIsRunning;
 
 public class KubernetesAbstractTest {
 
-    protected CoreV1Api api;
+    protected CoreV1Api k8sClient;
     protected String chart;
     protected String repo;
 
@@ -21,25 +25,32 @@ public class KubernetesAbstractTest {
     }
 
     private void getEnvVars() {
-        this.chart = System.getenv("CHART");
+        chart = System.getenv("CHART");
         if (chart == null) {
-            this.chart = "xap";
+            chart = "xap";
         }
 
-        this.repo = System.getenv("REPO_NAME");
-        if (this.repo == null) {
-            this.repo = "gigaspaces";
+        repo = System.getenv("REPO_NAME");
+        if (repo == null) {
+            repo = "gigaspaces";
         }
     }
 
     private void setK8Api() throws IOException {
         Configuration.setDefaultApiClient(Config.fromConfig("/home/eladg/.kube/config"));
-        api = new CoreV1Api();
+        k8sClient = new CoreV1Api();
     }
 
+    @BeforeEach
     @AfterEach
-    public void tearDown() {
+    public void cleanCluster() {
         removeAllChartsInstances();
+        assertNoRunningPods(k8sClient);
+    }
+
+    protected void installChartAndValidate(String chart, String deploymentName, String... args) {
+        installChartWithArgs(repo, chart, deploymentName, args);
+        assertPodsIsRunning(k8sClient, deploymentName);
     }
 
 }
